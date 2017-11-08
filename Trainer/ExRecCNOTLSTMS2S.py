56db2fa4c41761ba8800c725bc8b1f0158881478
Pooya Ronagh <p.ronagh@gmail.com>
2017-11-08 14:19:11 -0500

    A new sequence to sequence training for LSTM and ExRecCNOT.
    For example Z4 error, depends on Z1, Z2 and then Z4. Even though
    Z1 and Z2 do not have temporal precedence over each other, this new
    model allows for keeping the output sequence of each LSTM unit small
    (still 128 1-hot encodings). There are 3 replications of LSTM over
    time now.

--- Trainer/ExRecCNOTLSTMS2S.py
+++ Trainer/ExRecCNOTLSTMS2S.py
@@ -1,6 +1,7 @@
 # ------------------------------------------------------------------------------
 # 
-#    CNOTExRec trainer. Uses an RNN w/ 4 LSTM cells to train X & Z at same time.
+#    CNOTExRec trainer. Use 4 RNNs with 3 LSTM cells to train X & Z using
+#    a causal model for temporal sequences of syndrome and error.
 #
 #    Copyright (C) 2017 Pooya Ronagh
 # 
@@ -11,41 +12,57 @@ import numpy as np
 import tensorflow as tf
 import sys
 from util import y2indicator
+import threading
+import sys
+import os
+import json
+from time import localtime, strftime, clock
+import bayesopt
+from bayesoptmodule import BayesOptContinuous, BayesOptDiscrete
+
+# %matplotlib inline
+import matplotlib
+import matplotlib.pyplot as plt
 
 # The CSS code generator matrix
 G= np.matrix([[0,0,0,1,1,1,1], \
               [0,1,1,0,0,1,1], \
               [1,0,1,0,1,0,1]]).astype(np.int32)
+error_keys= ['X3', 'X4', 'Z3', 'Z4']
+all_keys= ['X1', 'X2', 'X3', 'X4',\
+           'Z1', 'Z2', 'Z3', 'Z4']
+temp_dic= {'X3': ['X1', 'X2', 'X3'],\
+           'X4': ['X1', 'X2', 'X4'],\
+           'Z3': ['Z1', 'Z2', 'Z3'],\
+           'Z4': ['Z1', 'Z2', 'Z4']}
 
 class Data:
 
-    def __init__(self, data):
+    def __init__(self, syn_data, err_data):
         self.input= {}
-        self.input['X']= \
-    np.concatenate((data['synX12'], data['synX34']), axis= 1).reshape(-1, 2, 6)
-        self.input['Z']= \
-    np.concatenate((data['synZ12'], data['synZ34']), axis= 1).reshape(-1, 2, 6)
         self.output= {}
-        self.output['X3']= data['errX3']
-        self.output['X4']= data['errX4']
-        self.output['Z3']= data['errZ3']
-        self.output['Z4']= data['errZ4']
-        X12_ind=y2indicator(data['errX12'],2**14).astype(np.float32)
-        X34_ind=y2indicator(data['errX34'],2**14).astype(np.float32)
-        Z12_ind=y2indicator(data['errZ12'],2**14).astype(np.float32)
-        Z34_ind=y2indicator(data['errZ34'],2**14).astype(np.float32)
         self.output_ind= {}
-        self.output_ind['X']= \
-    np.concatenate((X12_ind, X34_ind), axis= 1).reshape(-1, 2 * 2**14)
-        self.output_ind['Z']= \
-    np.concatenate((Z12_ind, Z34_ind), axis= 1).reshape(-1, 2 * 2**14)
+        self.output= {}
+        self.output_ind= {}
+        for key in error_keys:
+            self.input[key]= np.concatenate(\
+                tuple([syn_data[temp_dic[key][i]] for i in range(3)]), \
+                axis = 1).reshape(-1, 3, 3)
+        for key in error_keys:
+            self.output[key]= err_data[key]
+        for key in error_keys:
+            self.output_ind[key]= np.concatenate(tuple(\
+            [y2indicator(err_data[temp_dic[key][i]],2**7).astype(np.float32) \
+            for i in range(3)]), axis= 1).reshape(-1, 3, 2**7)
 
-def io_data_factory(data, test_size):
+def io_data_factory(syn_data, err_data, test_size):
 
-    train_data_arg = {key:data[key][:-test_size,] for key in data.keys()}
-    test_data_arg  = {key:data[key][-test_size:,] for key in data.keys()}
-    train_data = Data(train_data_arg)
-    test_data = Data(test_data_arg)
+    train_syn_data= {key:syn_data[key][:-test_size,] for key in syn_data.keys()}
+    train_err_data= {key:err_data[key][:-test_size,] for key in err_data.keys()}
+    test_syn_data = {key:syn_data[key][-test_size:,] for key in syn_data.keys()}
+    test_err_data = {key:err_data[key][-test_size:,] for key in err_data.keys()}
+    train_data = Data(train_syn_data, train_err_data)
+    test_data = Data(test_syn_data, test_err_data)
     return train_data, test_data
 
 def find_logical_fault(recovery, err):
@@ -61,37 +78,23 @@ def find_logical_fault(recovery, err):
     logical_err= np.sum(coset) % 2
     return logical_err
 
-def num_logical_fault(prediction, test_data, test_size):
+def num_logical_fault(prediction, test):
 
-    error_keys= ['X3', 'X4', 'Z3', 'Z4']
     error_counter= 0.0
-    for i in range(test_size):
-        pred= {}
-        pred['X3']= prediction['X'][i] // 2**7
-        pred['X4']= prediction['X'][i] % 2**7
-        pred['Z3']= prediction['Z'][i] // 2**7
-        pred['Z4']= prediction['Z'][i] % 2**7
+    for i in range(len(prediction[error_keys[0]])):
         for key in error_keys:
-            if (find_logical_fault(pred[key], test_data.output[key][i])):
+            if (find_logical_fault(prediction[key][i], test.output[key][i])):
                 error_counter+=1
                 break
-    return error_counter/test_size
-    
+    return error_counter/len(prediction[error_keys[0]])
+
 def get_data(filename):
 
-    data= {}
-    data['synX12']= []
-    data['synX34']= []
-    data['synZ12']= []
-    data['synZ34']= []
-    data['errX12']= []
-    data['errX34']= []
-    data['errZ12']= []
-    data['errZ34']= []
-    data['errX3']= []
-    data['errX4']= []
-    data['errZ3']= []
-    data['errZ4']= []
+    syn_data= {}
+    err_data= {}
+    for key in all_keys:
+        syn_data[key]= []
+        err_data[key]= []
     with open(filename) as file:
         first_line = file.readline();
         p, lu_avg, lu_std, data_size = first_line.split(' ')
@@ -101,136 +104,171 @@ def get_data(filename):
         data_size= int(data_size)
         for line in file.readlines():
             line_list= line.split(' ')
-            data['synX12'].append([bit for bit in ''.join(line_list[0:2])])
-            data['synX34'].append([bit for bit in ''.join(line_list[2:4])])
-            data['synZ12'].append([bit for bit in ''.join(line_list[8:10])])
-            data['synZ34'].append([bit for bit in ''.join(line_list[10:12])])
-            data['errX3'].append([int(line_list[6],2)])
-            data['errX4'].append([int(line_list[7],2)])
-            data['errZ3'].append([int(line_list[14],2)])
-            data['errZ4'].append([int(line_list[15],2)])
-            data['errX12'].append([int(''.join(line_list[4:6]),2)])
-            data['errX34'].append([int(''.join(line_list[6:8]),2)])
-            data['errZ12'].append([int(''.join(line_list[12:14]),2)])
-            data['errZ34'].append([int(''.join(line_list[14:16]),2)])
-    for key in data.keys():
-        data[key]= np.array(data[key]).astype(np.float32)
-    return data, p, lu_avg, lu_std, data_size
+            for i in range(0, 4):
+                syn_data['X'+str(i+1)].append([x for x in''.join(line_list[i])])
+            for i in range(4, 8):
+                err_data['X'+str(i-3)].append([int(line_list[i], 2)])
+            for i in range(8, 12):
+                syn_data['Z'+str(i-7)].append([z for z in''.join(line_list[i])])
+            for i in range(12, 16):
+                err_data['Z'+str(i-11)].append([int(line_list[i], 2)])
+    for key in all_keys:
+        syn_data[key]= np.array(syn_data[key]).astype(np.float32)
+        err_data[key]= np.array(err_data[key]).astype(np.float32)
+    return syn_data, err_data, p, lu_avg, lu_std, data_size
 
-def train(filename, param):
+def train(param, train_data, test_data, \
+          num_classes, num_inputs, input_size, n_batches):
 
-    test_fraction= param['data']['test fraction']
-    batch_size= param['data']['batch size']
+    prediction= {}
+    verbose= param['usr']['verbose']
+    batch_size= param['opt']['batch size']
     learning_rate= param['opt']['learning rate']
     num_iterations= param['opt']['iterations']
     momentum_val= param['opt']['momentum']
     decay_rate= param['opt']['decay']
-    verbose= param['usr']['verbose']
     num_hidden= param['nn']['num hidden'] 
+    W_std= param['nn']['W std'] 
+    b_std= param['nn']['b std'] 
 
-    output= {}
-    output['data']= {}
-    output['opt']= {}
-    output['res']= {}
+    # define all parts of the tf graph
+    tf.reset_default_graph()
+    x = {}
+    y = {}
+    lstm = {}
+    lstmOut = {}
+    W= {}
+    b= {}
+    logits= {}
+    loss= {}
+    predict= {}
+    
+    for key in error_keys:
+        with tf.variable_scope(key):
 
-    # Read data and figure out how much null syndromes to assume for error_scale
+            x[key] = tf.placeholder(tf.float32, [None, num_inputs, input_size])
+            y[key] = tf.placeholder(tf.float32, [None, num_inputs, num_classes])
+            lstm[key] = tf.contrib.rnn.LSTMCell(num_hidden)
+            lstmOut[key],_=tf.nn.dynamic_rnn(lstm[key],x[key], dtype=tf.float32)
+            W[key]= {}
+            b[key]= {}
+            logits[key]= []
+            loss[key]= []
+            for i in range(num_inputs):
+                W[key][i]= tf.Variable(\
+                    tf.random_normal([num_hidden,num_classes], stddev=W_std))
+                b[key][i]= tf.Variable(\
+                    tf.random_normal([num_classes], stddev=b_std))
+                logits[key].append(\
+                    tf.matmul(lstmOut[key][:,i,:], W[key][i]) + b[key][i])
+                loss[key].append(\
+                    tf.nn.softmax_cross_entropy_with_logits(\
+                        logits=logits[key][i], labels=y[key][:,i,:]))
+            predict[key]= tf.argmax(logits[key][-1], 1)
+    
+    cost= tf.reduce_mean(\
+        sum(loss[key][i] for key in error_keys for i in range(num_inputs)))
+    train = tf.train.RMSPropOptimizer(\
+        learning_rate, decay=decay_rate, momentum=momentum_val).minimize(cost)
+    init = tf.global_variables_initializer()
+    costs= []
+
+    with tf.Session() as session:
+        if (verbose): print('session begins '),
+        session.run(init)
+
+        for i in range(num_iterations):
+            if (verbose): print('.'),
+
+            for j in range(n_batches):
+                beg= j * batch_size
+                end= j * batch_size + batch_size
+                feed_dict={}
+                for key in error_keys:
+                    feed_dict[x[key]]= train_data.input[key][beg:end,]
+                    feed_dict[y[key]]= train_data.output_ind[key][beg:end,]
+                session.run(train, feed_dict)
+
+            feed_dict={}
+            for key in error_keys:
+                feed_dict[x[key]]= test_data.input[key]
+                feed_dict[y[key]]= test_data.output_ind[key]
+            test_cost = session.run(cost, feed_dict)
+            costs.append(test_cost)
+
+        for key in error_keys:
+            prediction[key] = session.run(predict[key], \
+                feed_dict= {x[key]: test_data.input[key]})
+        if (verbose): print(' session ends.')
+
+    plt.plot(costs)
+    plt.show()
+    return num_logical_fault(prediction, test_data)
+
+
+
+### Run an entire benchmark
+
+param= {}
+param['nn']= {}
+param['opt']= {}
+param['data']= {}
+param['usr']= {}
+param['nn']['num hidden']= 500
+param['nn']['W std']= 0.01
+param['nn']['b std']= 0.0
+param['opt']['batch size']= 1000
+param['opt']['learning rate']= 0.005
+param['opt']['iterations']= 10
+param['opt']['momentum']= 0.98
+param['opt']['decay']= 0.98
+param['data']['test fraction']= 0.1
+param['usr']['verbose']= True
+
+verbose= param['usr']['verbose']
+output= []
+num_classes= 2**7
+num_inputs= 3
+input_size= 3
+
+datafolder= './'
+file_list= os.listdir(datafolder)
+file_list= ['test.txt']
+
+for filename in file_list:
+    # Read data and find how much null syndromes to assume for error_scale
     print("Reading data from " + filename)
-    output['data']['path']= filename
+    syn_data, err_data, p, lu_avg, lu_std, data_size = \
+        get_data(datafolder + filename)
 
-    raw_data, p, lu_avg, lu_std, data_size = get_data(filename)
-    output['res']['p']= p
-    output['res']['lu avg']= lu_avg
-    output['res']['lu std']= lu_std
-
-    total_size= np.shape(raw_data['synX12'])[0]
+    test_fraction= param['data']['test fraction']
+    total_size= np.shape(syn_data['X1'])[0]
     test_size= int(test_fraction * total_size)
+    train_data, test_data = io_data_factory(syn_data, err_data, test_size)
+
+    batch_size= param['opt']['batch size']
     train_size= total_size - test_size
     n_batches = train_size // batch_size
     error_scale= 1.0*total_size/data_size
-    output['data']['fault scale']= error_scale
-    output['data']['total data size']= total_size
-    output['data']['test set size']= test_size
-    output['opt']['batch size']= batch_size
-    output['opt']['number of batches']= n_batches
 
-    num_classes= 2**14
-    num_inputs= 2
-    input_size= 6
+    avg= train(param, train_data, test_data, \
+        num_classes, num_inputs, input_size, n_batches)
 
-    train_data, test_data = io_data_factory(raw_data, test_size)
+    run_log= {}
+    run_log['data']= {}
+    run_log['opt']= {}
+    run_log['res']= {}
+    run_log['data']['path']= filename
+    run_log['data']['fault scale']= error_scale
+    run_log['data']['total data size']= total_size
+    run_log['data']['test set size']= test_size
+    run_log['opt']['batch size']= batch_size
+    run_log['opt']['number of batches']= n_batches
+    run_log['res']['p']= p
+    run_log['res']['lu avg']= lu_avg
+    run_log['res']['lu std']= lu_std
+    run_log['res']['nn avg'] = error_scale * avg
+    run_log['res']['nn std'] = 0
+    output.append(run_log)
 
-    prediction= {}
-    train_keys= ['X', 'Z']
-    for key in train_keys:
-        tf.reset_default_graph()
-
-        x = tf.placeholder(tf.float32, [None, num_inputs, input_size])
-        y = tf.placeholder(tf.float32, [None, num_inputs, num_classes])
-        lstm = tf.contrib.rnn.LSTMCell(num_hidden)
-        lstmOut, _ = tf.nn.dynamic_rnn(lstm, x, dtype=tf.float32)
-        lstmOut= tf.reshape(lstmOut, [-1, num_inputs * num_hidden])
-        W= tf.Variable(tf.random_normal([num_inputs * num_hidden, \
-            num_inputs * num_classes]))
-        b= tf.Variable(tf.random_normal([num_inputs * num_classes]))
-        logits= tf.matmul(lstmOut, W) + b
-    
-        loss= tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y)
-        cost= tf.reduce_sum(loss)
-
-        train = tf.train.RMSPropOptimizer(learning_rate, \
-            decay=decay_rate, momentum=momentum_val).minimize(cost)
-
-        predict= tf.argmax(logits[:, -num_classes:], 1)
-        init = tf.global_variables_initializer()
-
-        with tf.Session() as session:
-            session.run(init)
-
-            for i in range(num_iterations):
-                for j in range(n_batches):
-                    beg= j * batch_size
-                    end= j * batch_size + batch_size
-                    
-                    feed_dict={}
-                    feed_dict[x]= train_data.input[key][beg:end,]
-                    feed_dict[y]= train_data.output_ind[key][beg:end,]
-                    session.run(train, feed_dict)
-            
-            prediction[key]= session.run(predict, \
-                feed_dict= {x: test_data.input[key]})
-
-    avg= num_logical_fault(prediction, test_data, test_size)
-
-    output['res']['nn avg'] = error_scale * avg
-    output['res']['nn std'] = 0
-
-    return output
-
-'''
-__main__():
-  Args: 
-    json parameter file,
-    data folder.
-'''
-
-if __name__ == '__main__':
-
-    import sys
-    import os
-    import json
-    from time import localtime, strftime
-
-    with open(sys.argv[1]) as paramfile:
-        param = json.load(paramfile)
-    datafolder= sys.argv[2]
-
-    output= []
-
-    for filename in os.listdir(datafolder):
-        output.append(train(datafolder + filename, param))
-
-    outfilename = strftime("%Y-%m-%d-%H-%M-%S", localtime())
-    f = open('Reports/' + outfilename + '.json', 'w')
-    f.write(json.dumps(output, indent=2))
-    f.close()
-
+print(json.dumps(output, indent=2))
