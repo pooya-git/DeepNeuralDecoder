bfc69b1d5a8b13743238971b76ffd6357ce2f975
Pooya Ronagh <p.ronagh@gmail.com>
2017-11-10 01:13:00 -0500

    Jupyter notebook for surface code and bug fix.
    - Small indexing bug fixed from the python code.
    - Notebook created for the python code.

--- Trainer/SurfaceD3LSTM.py
+++ Trainer/SurfaceD3LSTM.py
@@ -145,7 +145,7 @@ def get_data(filename):
             data['synX'].append([bit for bit in ''.join(line_list[0:3])])
             data['synZ'].append([bit for bit in ''.join(line_list[6:9])])
             data['errX3'].append([int(line_list[5],2)])
-            data['errZ3'].append([int(line_list[8],2)])
+            data['errZ3'].append([int(line_list[11],2)])
     for key in data.keys():
         data[key]= np.array(data[key]).astype(np.float32)
     return data, p, lu_avg, lu_std, data_size
@@ -213,92 +213,190 @@ def train(param, train_data, test_data, \
                     feed_dict[x[key]]= train_data.input[key][beg:end,]
                     feed_dict[y[key]]= train_data.output_ind[key][beg:end,]
                 session.run(train, feed_dict)
-
-            feed_dict={}
-            for key in err_keys:
-                feed_dict[x[key]]= test_data.input[key]
-                feed_dict[y[key]]= test_data.output_ind[key]
-            test_cost = session.run(cost, feed_dict)
-            costs.append(test_cost)
+            
+            if (verbose>1):
+                feed_dict={}
+                for key in err_keys:
+                    feed_dict[x[key]]= test_data.input[key]
+                    feed_dict[y[key]]= test_data.output_ind[key]
+                test_cost = session.run(cost, feed_dict)
+                costs.append(test_cost)
 
         for key in err_keys:
             prediction[key] = session.run(predict[key], \
                 feed_dict= {x[key]: test_data.input[key]})
         if (verbose): print(' session ends.')
 
-    plt.plot(costs)
-    plt.show()
+    if (verbose>1):
+        plt.plot(costs)
+        plt.show()
+        
     return num_logical_fault(prediction, test_data)
 
-### Run an entire benchmark
-# [-4.19456682,  0.85001536,  0.91390672,  5.00913479, -1.81279482]
-# 1. # 2. learning rate= 10**x[1] 
-# 2. # 4. momentum= x[3] 
-# 3. # 5. decay= x[4]
-# 4.  # 6. num hidden= int(x[5] * 10)
-# 5. # 7. W std= 10**x[6]
+class BayesOptTest(BayesOptContinuous):
 
-param= {}
-param['nn']= {}
-param['opt']= {}
-param['data']= {}
-param['usr']= {}
-param['nn']['num hidden']= 50
-param['nn']['W std']= 10.0**(-1.81279482)
-param['nn']['b std']= 0.0
-param['opt']['batch size']= 1000
-param['opt']['learning rate']= 10.0**(-4.19456682)
-param['opt']['iterations']= 20
-param['opt']['momentum']= 0.85001536
-param['opt']['decay']= 0.91390672
-param['data']['test fraction']= 0.1
-param['usr']['verbose']= True
- 
-verbose= param['usr']['verbose']
-output= []
-num_classes= 2**9
-num_inputs= 3
-input_size= 4
+    def __init__(self, N, filename, param,\
+            raw_data, p, lu_avg, lu_std, data_size):
+        super(BayesOptTest, self).__init__(N)
+        self.filename= filename
+        self.param= param
+        self.raw_data= raw_data
+        self.p= p
+        self.lu_avg= lu_avg
+        self.lu_std= lu_std
+        self.data_size= data_size
 
-datafolder= '../../Data/SurfaceD3/e-05/'
-file_list= os.listdir(datafolder)
+        self.test_fraction= self.param['data']['test fraction']
+        self.total_size= np.shape(self.raw_data['synX'])[0]
+        self.test_size= int(self.test_fraction * self.total_size)
+        self.train_data, self.test_data = \
+            io_data_factory(self.raw_data, self.test_size)
 
-for filename in file_list:
-    # Read data and find how much null syndromes to assume for error_scale
-    print("Reading data from " + filename)
+        self.batch_size= self.param['opt']['batch size']
+        self.train_size= self.total_size - self.test_size
+        self.n_batches = self.train_size // self.batch_size
+        self.error_scale= 1.0*self.total_size/self.data_size
+
+        self.num_classes= 2**9
+        self.num_inputs= 3
+        self.input_size= 4
+
+    def evaluateSample(self, x):
+        
+        print('new sample='+ str(x))
+        self.param['opt']['batch size']= int(x[0] * 100) 
+        self.param['opt']['learning rate']= 10**x[1] 
+        self.param['opt']['iterations']= int(x[2] * 10)
+        self.param['opt']['momentum']= x[3] 
+        self.param['opt']['decay']= x[4]
+        self.param['nn']['num hidden']= int(x[5] * 10)
+        self.param['nn']['W std']= 10**x[6]
+        self.param['nn']['b std']= 10**x[7]
+
+        avg = train(self.param, self.train_data, self.test_data, \
+            self.num_classes, self.num_inputs, self.input_size, self.n_batches)
+        print('result= '+ str(self.error_scale * avg))
+        return self.error_scale * avg
+
+# Run hypertuning
+def hypertune():
+
+    lb = np.array([10.0, -6.0, 100., 0.8, 0.8, 10, -4.0, -4.0])
+    ub = np.array([10.0, -4.0, 100., 0.99, 0.99, 10, 0.9,-2.0])
+
+    param= {}
+    param['nn']= {}
+    param['opt']= {}
+    param['data']= {}
+    param['usr']= {}
+    param['nn']['num hidden']= 500
+    param['nn']['W std']= 0.0
+    param['nn']['b std']= 0.0
+    param['opt']['batch size']= 1000
+    param['opt']['learning rate']= 10e-6
+    param['opt']['iterations']= 20
+    param['opt']['momentum']= 0.9
+    param['opt']['decay']= 0.9
+    param['data']['test fraction']= 0.1
+    param['usr']['verbose']= 2
+
+    datafolder= '../SurfaceData/e-04/'
+    filename= 'SyndromeAndError2.000e-04.txt'
     raw_data, p, lu_avg, lu_std, data_size = get_data(datafolder + filename)
 
-    test_fraction= param['data']['test fraction']
-    total_size= np.shape(raw_data['synX'])[0]
-    test_size= int(test_fraction * total_size)
-    train_data, test_data = io_data_factory(raw_data, test_size)
+    engine = BayesOptTest(len(lb), datafolder + filename, param,\
+        raw_data, p, lu_avg, lu_std, data_size)
 
-    batch_size= param['opt']['batch size']
-    train_size= total_size - test_size
-    n_batches = train_size // batch_size
-    error_scale= 1.0*total_size/data_size
+    hyperparam = {}
+    hyperparam['n_iterations'] = 50
+    hyperparam['n_iter_relearn'] = 5
+    hyperparam['n_init_samples'] = 2
+    hyperparam['noise']= 1e-10
+    # hyperparam['l_type']= 'L_MCMC'
+    hyperparam['kernel_name'] = "kMaternARD5"
+    hyperparam['kernel_hp_mean'] = [1]
+    hyperparam['kernel_hp_std'] = [5]
+    hyperparam['surr_name'] = "sStudentTProcessNIG"
+    #hyperparam['crit_name'] = "cMI"
 
-    avg= train(param, train_data, test_data, \
-        num_classes, num_inputs, input_size, n_batches)
+    engine.parameters = hyperparam
+    engine.lower_bound = lb
+    engine.upper_bound = ub
+    start = clock()
+    mvalue, x_out, error = engine.optimize()
+    print("Result", mvalue, "at", x_out)
+    print("Running time:", clock() - start, "seconds")
+# Run an entire benchmark
+if __name__ == '__main__':
 
-    run_log= {}
-    run_log['data']= {}
-    run_log['opt']= {}
-    run_log['res']= {}
-    run_log['data']['path']= filename
-    run_log['data']['fault scale']= error_scale
-    run_log['data']['total data size']= total_size
-    run_log['data']['test set size']= test_size
-    run_log['opt']['batch size']= batch_size
-    run_log['opt']['number of batches']= n_batches
-    run_log['res']['p']= p
-    run_log['res']['lu avg']= lu_avg
-    run_log['res']['lu std']= lu_std
-    run_log['res']['nn avg'] = error_scale * avg
-    run_log['res']['nn std'] = 0
-    output.append(run_log)
+    param= {}
+    param['nn']= {}
+    param['opt']= {}
+    param['data']= {}
+    param['usr']= {}
+    param['nn']['num hidden']= 500
+    param['opt']['batch size']= 1000
+    param['data']['test fraction']= 0.1
+    param['opt']['iterations']= 30
+    param['usr']['verbose']= 0
 
-outfilename = strftime("%Y-%m-%d-%H-%M-%S", localtime())
-f = open('../Reports/SurfaceD3/' + outfilename + '.json', 'w')
-f.write(json.dumps(output, indent=2))
-f.close()
+    x= [-5.69398902, 0.9, 0.9, 50., -2.14472344]
+    param['opt']['learning rate']= 10**x[0]
+    param['opt']['momentum']= x[1] 
+    param['opt']['decay']= x[2]
+    param['nn']['num hidden']= int(x[3] * 10)
+    param['nn']['W std']= 10**x[4]
+    param['nn']['b std']= 0.0
+
+    output= []
+    num_classes= 2**9
+    num_inputs= 3
+    input_size= 4
+    verbose= param['usr']['verbose']
+
+    datafolder= '../SurfaceData/e-04/'
+    file_list= os.listdir(datafolder)
+
+    for filename in file_list:
+        # Read data and find how much null syndromes to assume for error_scale
+        print("Reading data from " + filename)
+        raw_data, p, lu_avg, lu_std, data_size = get_data(datafolder + filename)
+
+        test_fraction= param['data']['test fraction']
+        total_size= np.shape(raw_data['synX'])[0]
+        test_size= int(test_fraction * total_size)
+        train_data, test_data = io_data_factory(raw_data, test_size)
+
+        batch_size= param['opt']['batch size']
+        train_size= total_size - test_size
+        n_batches = train_size // batch_size
+        error_scale= 1.0*total_size/data_size
+
+        avg= train(param, train_data, test_data, \
+            num_classes, num_inputs, input_size, n_batches)
+
+        run_log= {}
+        run_log['data']= {}
+        run_log['opt']= {}
+        run_log['res']= {}
+        run_log['data']['path']= filename
+        run_log['data']['fault scale']= error_scale
+        run_log['data']['total data size']= total_size
+        run_log['data']['test set size']= test_size
+        run_log['opt']['batch size']= batch_size
+        run_log['opt']['number of batches']= n_batches
+        run_log['res']['p']= p
+        run_log['res']['lu avg']= lu_avg
+        run_log['res']['lu std']= lu_std
+        run_log['res']['nn avg'] = error_scale * avg
+        run_log['res']['nn std'] = 0
+        output.append(run_log)
+        
+        if (verbose):
+            print('fault rate= '+str(error_scale * avg))
+
+    outfilename = strftime("%Y-%m-%d-%H-%M-%S", localtime())
+    f = open('../Reports/SurfaceD3/' + outfilename + '.json', 'w')
+    f.write(json.dumps(output, indent=2))
+    f.close()
+
