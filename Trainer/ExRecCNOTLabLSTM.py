d8168604c9b661fe9d69f65aeebf80b7ead96237
Pooya Ronagh <p.ronagh@gmail.com>
2017-11-13 21:25:21 -0500

    Results of the Lab scenario ExRecCNOT.

--- Trainer/ExRecCNOTLabLSTM.py
+++ Trainer/ExRecCNOTLabLSTM.py
@@ -1,6 +1,6 @@
 # ------------------------------------------------------------------------------
 # 
-#    CNOTExRec trainer. Use 4 RNNs with 1 LSTM cell to train X & Z at same time.
+#    CNOTExRec trainer. Use 4 RNNs with 1 LSTM cell for the Lab scenario.
 #
 #    Copyright (C) 2017 Pooya Ronagh
 # 
@@ -17,7 +17,7 @@ import os
 import json
 from time import localtime, strftime, clock
 import cPickle as pickle
-import ExRecCNOTData
+from ExRecCNOTData import *
 
 def train(param, train_data, test_data, \
           num_classes, num_inputs, input_size, n_batches):
@@ -101,3 +101,70 @@ def train(param, train_data, test_data, \
         plt.show()
 
     return num_logical_fault(prediction, test_data)
+
+if __name__ == '__main__':
+
+    param= {}
+    param['nn']= {}
+    param['opt']= {}
+    param['data']= {}
+    param['usr']= {}
+    param['nn']['num hidden']= 500
+    param['nn']['W std']= 10.0**(-0.95849325)
+    param['nn']['b std']= 0.0
+    param['opt']['batch size']= 1000
+    param['opt']['learning rate']= 10.0**(-4.55693412)
+    param['opt']['iterations']= 10
+    param['opt']['momentum']= 0.99
+    param['opt']['decay']= 0.98
+    param['data']['test fraction']= 0.1
+    param['usr']['verbose']= True
+    param['nn']['type']= 'ExRecCNOTLabLSTM'
+
+    verbose= param['usr']['verbose']
+    test_fraction= param['data']['test fraction']
+    output= []
+    num_classes= 2
+    num_inputs= 2
+    input_size= 6
+
+    datafolder= '../Data/CNOTPkl/e-04/'
+    file_list= os.listdir(datafolder)
+
+    count= 0
+    for filename in file_list:
+        if count>5: break
+        else: count+=1
+
+        with open(datafolder + filename, 'rb') as input_file:
+            print("Pickling model from " + filename)
+            m = pickle.load(input_file)
+        
+        batch_size= param['opt']['batch size']
+        n_batches = m.train_size // batch_size
+
+        avg= train(param, m.train_data, m.test_data, \
+            num_classes, num_inputs, input_size, n_batches)
+
+        run_log= {}
+        run_log['data']= {}
+        run_log['opt']= {}
+        run_log['res']= {}
+        run_log['param']= param
+        run_log['data']['path']= filename
+        run_log['data']['fault scale']= m.error_scale
+        run_log['data']['total data size']= m.total_size
+        run_log['data']['test set size']= m.test_size
+        run_log['opt']['batch size']= batch_size
+        run_log['opt']['number of batches']= n_batches
+        run_log['res']['p']= m.p
+        run_log['res']['lu avg']= m.lu_avg
+        run_log['res']['lu std']= m.lu_std
+        run_log['res']['nn avg'] = m.error_scale * avg
+        run_log['res']['nn std'] = 0
+        output.append(run_log)
+
+    outfilename = strftime("%Y-%m-%d-%H-%M-%S", localtime())
+    f = open('../Reports/CNOTLab/' + outfilename + '.json', 'w')
+    f.write(json.dumps(output, indent=2))
+    f.close()
