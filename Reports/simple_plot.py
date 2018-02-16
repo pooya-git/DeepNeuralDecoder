51a8bc3f19f175fec791e2ff387d7288aacb217d
Pooya <p.ronagh@gmail.com>
2018-02-16 17:15:05 -0500

    The LU-RNN surface code result. Simple plotting tool added.
    - Old multiplot.py is renamed to fancy_plot.py.
    - The newly added simple_plot.py avoids reclined equations for polynomial
    curve fitting.
    - The last surface code d=5 result is added.
    - Plot commands are updated.

---
+++ Reports/simple_plot.py
@@ -0,0 +1,99 @@
+import sys, os, json
+import matplotlib.pyplot as plt
+import matplotlib
+from pylab import *
+from scipy.optimize import curve_fit
+from numpy import arctan, pi, log, exp
+
+matplotlib.rcParams.update({'font.size': 14})
+
+def poly(x, a):
+    global poly_deg
+    return a * np.power(x, poly_deg)
+
+def plot_results(filenames, titles= None):
+
+	global poly_deg
+	fig = figure(figsize=(9, 6))
+	ax = fig.add_subplot(111)
+	ax.set_yscale('log')
+	ax.set_xscale('log')
+	lightnavy = matplotlib.colors.colorConverter.to_rgb('#0174DF') 
+	darknavy = matplotlib.colors.colorConverter.to_rgb('#084B8A')
+	ax.yaxis.grid(True, linestyle='-', which='major', color='grey')
+	ax.yaxis.grid(True, linestyle='-', which='minor', color='lightgrey')
+	ax.xaxis.grid(True, linestyle='-', which='major', color='grey')
+	ax.xaxis.grid(True, linestyle='-', which='minor', color='lightgrey')
+	ax.set_xlabel('Physical fault rate')
+	ax.set_ylabel('Logical fault rate')
+	plt.rc('lines', linewidth= 3)
+	plt.rc('lines', linestyle= 'none')
+	plt.rc('axes', prop_cycle=(cycler('color', ['r', 'g', 'b', 'y'])))
+
+	with open(filenames[0]) as data_file:
+		res = json.load(data_file)
+	lu_avg= [elt['res']['lu avg'] for elt in res]
+	lu_std= [elt['res']['lu std'] for elt in res]
+	for i in range(len(lu_std)):
+		if lu_std[i]==0.00012393: lu_std[i]= 0.000012393
+	upper= [elt['data']['fault scale'] for elt in res]
+	p= [elt['res']['p'] for elt in res]
+	p, lu_avg, lu_std, upper= zip(*sorted(zip(p, lu_avg, lu_std, upper)))
+
+	lu_poly, _ = curve_fit(poly, p, lu_avg)
+	g= ax.plot(p, lu_avg, \
+		marker='o', markersize=4, \
+		label= 'Look up table' + ' ($%.2e p^%d$)' % (lu_poly[0], poly_deg))
+	ax.errorbar(p, lu_avg, yerr=lu_std, \
+		color= g[-1].get_color(), linestyle='None', \
+		markersize=9, capsize=4)
+	g= ax.plot(p, poly(p, *lu_poly), \
+		color= g[-1].get_color(), linestyle= '--')
+
+	plt.plot(p, p, linestyle= '--', color= 'black', linewidth= 2)
+
+	if titles:
+		title_iter= iter(titles)
+	for file in filenames:
+		with open(file) as data_file:
+			res = json.load(data_file)
+		nn_avg= [elt['res']['nn avg'] if 'nn avg' in elt['res'].keys() else \
+			np.mean(elt['res']['nn res']) for elt in res]
+		nn_std= [elt['res']['nn std'] if 'nn std' in elt['res'].keys() else \
+			np.std(elt['res']['nn res']) for elt in res]
+		p= [elt['res']['p'] for elt in res]
+		p, nn_avg, nn_std = zip(*sorted(zip(p, nn_avg, nn_std)))
+		if not titles:
+			label= res[0]['param']['nn']['type'] \
+				 + str(len(res[0]['param']['nn']['num hidden']))
+		else:
+			label= next(title_iter)
+		nn_poly, _ = curve_fit(poly, p, nn_avg)
+		g= ax.plot(p, nn_avg, \
+			marker='o', markersize=4, \
+			label= label + ' ($%.2e p^%d$) ' % (nn_poly[0], poly_deg))
+		ax.errorbar(p, nn_avg, yerr=nn_std, \
+			color= g[-1].get_color(), linestyle='None', \
+			markersize=9, capsize=4)
+		g= ax.plot(p, poly(p, *nn_poly), \
+			color= g[-1].get_color(), linestyle= '--')
+
+	plt.legend()
+	plt.show()
+
+if __name__ == '__main__':
+
+	poly_deg= int(sys.argv[2])
+	global poly_deg
+	if os.path.isdir(sys.argv[2]):
+		filenames= []
+		for elt in os.listdir(sys.argv[3]):
+			path= os.path.join(sys.argv[3], elt)
+			if not os.path.isdir(path):
+				filenames.append(path)
+		print filenames
+		plot_results(filenames)
+	else:
+		filenames= sys.argv[3::3]
+		titles= sys.argv[4::3]
+		plot_results(filenames, titles)
